123bf810799a75def0eed8fd1e5a5e0fe955bb2c
shihadkp <mshihadkp7@gmail.com>
2026-09-01 11:11:19 +0530

    meta pixel setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { Preloader } from "@/components/preloader";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { MetaPixel } from "@/components/MetaPixel";
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -142,6 +143,7 @@ export default function RootLayout({
         <Preloader />
         {children}
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
+        <MetaPixel />
       </body>
     </html>
   );
